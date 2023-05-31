e484806b053ee51a31c296bd277b12aa0bc49841
김진하 <128116382+KimJinHaa@users.noreply.github.com>
2023-05-31 16:40:12 +0900

    test10

--- readytomeet/src/main/java/com/example/demo/ReadytomeetApplication.java
+++ readytomeet/src/main/java/com/example/demo/ReadytomeetApplication.java
@@ -21,5 +21,7 @@ public class ReadytomeetApplication {
 		System.out.println("zzzzz");
 		System.out.println("zzzzz");
 		System.out.println("zzzzz");
+		System.out.println("zzzzz");
+		System.out.println("zzzzz");
 	}
 }
